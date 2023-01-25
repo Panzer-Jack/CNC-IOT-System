5f66b26da8cb9118f1d9e8d3d69113257cd3d70d
Panzer-Jack <81006731+Panzer-Jack@users.noreply.github.com>
2023-01-25 22:19:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 SECRET_KEY = "DAS<:WDIONIZCNISDSD_)+UWHAI"
 
 # 数据库的配置信息
-HOSTNAME = "127.0.0.1"
-PORT = 3306
-USERNAME = "root"
-PASSWORD = "123456123"
-DATABAEE = "zhihu_simple"
+HOSTNAME = ""
+PORT = 
+USERNAME = ""
+PASSWORD = ""
+DATABAEE = ""
 
 DB_URI = \
     f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:" \
@@ -19,6 +19,6 @@ SQLALCHEMY_DATABASE_URI = DB_URI
 MAIL_SERVER = "imap.exmail.qq.com"
 MAIL_USE_SSL = True
 MAIL_PORT = 465
-MAIL_USERNAME = "panzer_jack@panzer-jack.cn"
-MAIL_PASSWORD = "sHEN20020311"
-MAIL_DEFAULT_SENDER = "panzer_jack@panzer-jack.cn"
+MAIL_USERNAME = ""
+MAIL_PASSWORD = ""
+MAIL_DEFAULT_SENDER = "
